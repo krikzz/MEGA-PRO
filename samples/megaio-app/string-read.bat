5dc82143eeeb747757ebd11d8d8248a19dc88c66
krikzz <biokrik@gmail.com>
2024-11-27 19:03:09 +0100

    upd

--- samples/megaio-app/string-read.bat
+++ samples/megaio-app/string-read.bat
@@ -1,1 +1,1 @@
-E:\projects\MEGA-ED-PRO\_tools\megalink\megalink\bin\Release\megalink.exe -memwr string.txt 0x1810000
+megalink.exe -memwr string.txt 0x1810000
--- samples/megaio-app/usb.bat
+++ samples/megaio-app/usb.bat
@@ -1,1 +1,1 @@
-E:\projects\MEGA-ED-PRO\_tools\megalink\megalink\bin\Release\megalink.exe megaio.md
+megalink.exe megaio.md
